7c58541e6390c033ae31b7e4bbf5df2f48a4a65d
Arceline Cruz <cruzalacar@outlook.com>
2020-01-27 14:14:35 -0500

    added navbar & background

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Spinner from './Spinner';
 import NavBar from './NavBar';
+import './background.css';
 
 class App extends React.Component { 
 
     state = { nav: null };
 
     renderContent() {
-        return <NavBar />;
+        return (
+            
+            <NavBar />
+        );
     }
 
     render() {
         return (
-            <div>
+            <div className={`bgimg-1`}>
                 {this.renderContent()}
             </div>
         )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './navbar.css';
 
 class NavBar extends React.Component {
 
     render () {
         return(
-            <div className="following bar light fixed">
-               <div  className="ui container">
-                   <div className="ui large secondary menu">
-                        <div className="item">
-                            <div className="ui logo">
-                                Arceline Joy
-                            </div>
-                        </div>
-                        <a className="view-ui item">
-                            <i className="sidebar icon"></i>
-                            Menu
-                        </a>
-                        <a className="view-ui item">
-                            Home
-                        </a>
-                        <a className="view-ui item">
-                            Resume
-                        </a>
-                        <a className="view-ui item">
-                            Projects
-                        </a>
-                        <a className="view-ui item">
-                            Contact Me
-                        </a>
-                   </div>
-               </div>
+            <div className="navbar-following">
+                <a href="#home"> Arceline Joy </a>
+                <a href="#home"> Home </a>
+                <a href="#projects"> Projects </a>
+                <a href="#contact"> Contact Me </a>
             </div>
         );
     }
