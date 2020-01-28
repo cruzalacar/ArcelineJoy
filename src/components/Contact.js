69c7899679a217a0faaf8074d86b7a06c41a9394
Arceline Cruz <cruzalacar@outlook.com>
2020-01-27 19:38:44 -0500

    added missing files

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React, {Components} from 'react';
+
+class Contact extends React.Component{
+    render() {
+        return(
+            <div>Contact Me</div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Projects extends React.Component{
+    render() {
+        return (
+            <div>Projects Page</div>
+        );
+    }
+}
+
+export default Projects;
