791cc3209189a28a97a873a9d1bf94673ef1f3f5
Arceline Cruz <cruzalacar@outlook.com>
2020-01-27 19:22:47 -0500

    added social media links and background images

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Spinner from './Spinner';
-import NavBar from './NavBar';
+import React, {Component} from 'react';
+
 import './background.css';
+import NavBar from './NavBar';
+
+
+
 
 class App extends React.Component { 
 
-    state = { nav: null };
-
-    renderContent() {
-        return (
-            
-            <NavBar />
-        );
-    }
-
     render() {
         return (
-            <div className={`bgimg-1`}>
-                {this.renderContent()}
+            <div>
+                <NavBar />
+                <div className={`bgimg-1`}>
+                    <span>My name is Arceline Cruz</span>
+                    
+                    
+                    
+                </div>
+                <div className={`info-box`}>
+                    <div className={`info-box-name`}>
+                        Hello World
+                    </div>
+                </div>
+                <div className={`bgimg-2`}></div>
+                <div className={`info-box`}>
+                    <div className={`info-box-title`}>
+                        Skills & Technologies
+                        <div className={`info-box-text`}>
+                            C#
+                        </div>
+                    </div>
+                </div>
+                <div className={`bgimg-3`}></div>
+                <div className={`info-box`}>
+                    <div className={`info-box-title`}>
+                        Experience
+                        <div className={`info-box-text`}>
+
+                        </div>
+                    </div>
+                </div>
+                <div className={`bgimg-4`}></div>       
             </div>
-        )
+        );
     }
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './navbar.css';
+import {SocialIcon} from 'react-social-icons';
 
 class NavBar extends React.Component {
 
     render () {
         return(
             <div className="navbar-following">
-                <a href="#home"> Arceline Joy </a>
-                <a href="#home"> Home </a>
-                <a href="#projects"> Projects </a>
-                <a href="#contact"> Contact Me </a>
+                <div>
+                    <a href="#home"> Arceline Joy </a>
+                    <a href="#home"> Home </a>
+                    <a href="#projects"> Projects </a>
+                    <a href="#contact"> Contact Me </a>
+                </div>
+                <div className="icon">
+                    <SocialIcon url="https://github.com/cruzalacar" />
+                    <SocialIcon url="https://www.linkedin.com/in/arceline-joy-cruz-alacar-603358117/" />
+                </div>
             </div>
         );
     }
