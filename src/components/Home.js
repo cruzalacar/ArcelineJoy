d8ed2de5ef9c8bdf60a6c278fa1f8ab9449660a2
Arceline Cruz <cruzalacar@outlook.com>
2020-01-29 15:27:21 -0500

    Added all basic info to the homepage and updated the css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,69 +5,66 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <div className={`bgimg-1`}>
-                    <div className={`hello-box`} >
+                <div className="bgimg-1">
+                    <div className="hello-box" >
                         <h1> Hello, </h1>
                         <h1> I'm Arceline </h1>
                     </div>
                 </div>
-                <div className={`info-box-light`}>
+                <div className="info-box-light">
                     <h1>WORK IN PROGRESS</h1>
                     <p>
                         Hello, My name is Arceline Joy Cruz, aka AJ. I am a student attending the Bachelor of Mobile Computing at
-                        Sheridan
-                        </p>
-                </div>
-                <div className={`bgimg-2`}></div>
-                <div className={`info-box-dark`}>
-                    <p>
-                        Skills & Technologies
-
-                        Programming languages
-                        - C#, Python, Java, C, Swift
-
-                        Web Technologies
-                        - React, Javascript, CSS, HTML
-
-                        Database
-                        - MySQL, MS SQL Server, SQLite,
-
-                        APIs
-                        - Django
-
+                        Sheridan. I build this website using React.
                     </p>
                 </div>
-                <div className={`bgimg-3`}></div>
-                <div className={`info-box-light`}>
-                    <p>
-                        Education
-
-                        Applied Computer Science (Mobile Computing), Sheridan College, Oakville ON
-                        Expected: 2021
-                        - Focus on the developement of mobile and web applications for operating systems like Android and iOS.
-                        - Studied programming languages suh as C#, Java, Python & Swift.
-                        - Learned to use React, CSS and HTML for front end web development.
-
-                        Applied Computer Science (Information Security), Sheridan College, Oakville ON
-                        2014 - 2015
-                        - Focus on technological security on all devices regarding privacy through internet access.
-                        - Studied programming lanugages such as C and Java.
-                    </p>
+                <div className="bgimg-2">
+                    <div className="title-box">
+                        <h1>   Skills & Technologies</h1>
+                    </div>
                 </div>
-                <div className={`bgimg-4`}></div>
-                <div className={`info-box-dark`}>
-                    <p>
-                        Work Experience
-    
-                        Receptionist & Assistant, Guerrero Law Professional Corp, Brampton ON
-                        - Responded to real eastate inquires.
-                        - Arranged appointments with clients and set reminders for clients and employer
-                        - Knowledgeable in LawPro, Conveyancer, work & excel.
-    
-                        Customer Service & Order Entry, Fujifilm, Mississauga ON
-                        - Collected and updated data to assist with customer issues and concerns.
-                        - Tracked and documented all received requests in an incident management system.
-                    </p>
+                <div className="info-box-dark ">
+                    <h3>Programming Languages</h3>
+                    <p>C#, Python, Java, C & Swift</p>
+                    <h3>Web Technologies</h3>
+                    <p>React, Javascript, CSS & HTML</p>
+                    <h3>Database</h3>
+                    <p>MySQL, MS SQL Server & SQLite</p>
+                    <h3>APIs</h3>
+                    <p>Django</p>
+                </div>
+                <div className="bgimg-3">
+                    <div className="title-box">
+                        <h1>Education</h1>
+                    </div>
+                </div>
+                <div className="info-box-grey">
+                    <h3>Applied Computer Science (Mobile Computing), Sheridan College, Oakville ON</h3>
+                    <h3> Expected: 2021</h3>
+                    <p>• Focus on the developement of mobile and web applications for operating systems like Android and iOS.</p>
+                    <p>• Studied programming languages suh as C#, Java, Python & Swift.</p>
+                    <p>• Learned to use React, CSS and HTML for front end web development.</p>
+                    
+                    <h3>Applied Computer Science (Information Security), Sheridan College, Oakville ON</h3>
+                    <h3>2014 - 2015</h3>
+                    <p>• Focus on technological security on all devices regarding privacy through internet access.</p>
+                    <p>• Studied programming lanugages such as C and Java.</p>
+                </div>
+                <div className="bgimg-4">
+                    <div className="title-box">
+                        <h1>Work Experience</h1>
+                    </div>
+
+                </div>
+                <div className="info-box-dark">
+                    <h3>Receptionist & Assistant, Guerrero Law Professional Corp, Brampton ON</h3>
+                    <p>• Responded to real eastate inquires.</p>
+                    <p>• Arranged appointments with clients and set reminders for clients and employer.</p>
+                    <p>• Knowledgeable in LawPro, Conveyancer, work & excel.</p>
+                    
+                    <h3>Customer Service & Order Entry, Fujifilm, Mississauga ON</h3>
+                    <p>• Collected and updated data to assist with customer issues and concerns.</p>
+                    <p>• Tracked and documented all received requests in an incident management system.</p>
                 </div>
 
             </div>
