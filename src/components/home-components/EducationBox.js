b2011bb8af4f57e274886366b4575616e884cc3c
Arceline Cruz <cruzalacar@outlook.com>
2022-01-20 02:33:42 -0500

    Updated text files

--- src/components/home-components/EducationBox.js
+++ src/components/home-components/EducationBox.js
@@ -7,7 +7,7 @@ class EducationBox extends React.Component{
             <div>
                 <div className="info-box-grey">
                     <h3>Applied Computer Science (Mobile Computing), Sheridan College, Oakville ON</h3>
-                    <h3> Expected: 2021</h3>
+                    <h3> Expected: 2023</h3>
                     <p>• Focused on the developement of mobile and web developement for operating systems like Android and iOS.</p>
                     <p>• Studied programming languages suh as C#, Java, Python & Swift.</p>
                     <p>• Learned to use React, CSS and HTML for front end web development.</p>
--- src/components/home-components/ExperienceBox.js
+++ src/components/home-components/ExperienceBox.js
@@ -6,15 +6,16 @@ class ExperienceBox extends React.Component{
         return(
             <div>
                 <div className="info-box-dark">
-                    <h3>Receptionist & Assistant, Guerrero Law Professional Corp, Brampton ON</h3>
+                    <h3>Legal Assisant, Guerrero Law Professional Corp, Brampton ON</h3>
                     <p>• Arranged appointments with clients and set reminders for upcoming transactions for employer and clients.</p>
                     <p>• Responded to real estate inquiries from clients.</p>
                     <p>• Assisted with file preparation, such as opening files and organizing documents for the proper transaction.</p>
 
-                    <h3>Customer Service & Order Entry, Fujifilm, Mississauga ON</h3>
+                    <h3>Quality Assurance, UPS, Caledon ON</h3>
                     <p>• Tracked and documented all service requests in an incident management system.</p>
-                    <p>• Collected returned orders and updated the status report to ensure the issue is dealt with in a timely fashion.</p>
-                    <p>• Entered a new order’s information into an order management system using an RF scanner.</p>
+                    <p>• Collected returned packages and updated the status report to ensure the issue is dealt with in a timely fashion.</p>
+                    <p>• Entered a package information into an order management system using an RF scanner.</p>
+                    
                 </div>
             </div>
         );
